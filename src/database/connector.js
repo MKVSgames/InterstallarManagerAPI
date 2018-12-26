17a9a4ebbc7b82a1b390f64ada06ca51584dfd97
MartinKamenov <martinkamenov10@gmail.com>
2018-12-26 18:40:53 +0200

    Added connection object.

--- src/database/connector.js
+++ src/database/connector.js
@@ -1,5 +1,6 @@
 const Database = require('./mongodb');
-const connectionstring = require('./connectInfo');
+const connectionObject = require('./connectInfo');
+const connectionstring = `mongodb://${connectionObject.username}:${connectionObject.password}@ds143614.mlab.com:43614/interstellardb`;
 const database = new Database(connectionstring);
 
 module.exports = database;
