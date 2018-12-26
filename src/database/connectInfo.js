aad592635c7df0d2a50c0005b5f9d80289fdb4c4
MartinKamenov <martinkamenov10@gmail.com>
2018-12-26 18:49:05 +0200

    Added home route

---
+++ src/database/connectInfo.js
@@ -0,0 +1,7 @@
+const connectionObject = 
+{ 
+    username: 'doglover',
+    password: 'otednodoosem18'
+};
+
+module.exports = connectionObject;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const database = require('./database/connector');
+const startRoute = require('./routes/start/start-route');
 const worldsRoute = require('./routes/game/worlds/worlds-route');
 const WorldRepository = require('./models/repositories/worldRepository');
 
 const worldRepository = new WorldRepository(database, 'worlds');
 
+startRoute(app);
 worldsRoute(app, worldRepository);
 
 const port = process.env.PORT || 5000;
