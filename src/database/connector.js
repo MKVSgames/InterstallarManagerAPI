5917aa6bd3d7888ef4cb6fa3c55d28907b46df38
MartinKamenov <martinkamenov10@gmail.com>
2019-01-01 11:05:50 +0200

    Changed connection setup type.

--- src/database/connector.js
+++ src/database/connector.js
@@ -1,6 +1,7 @@
 const Database = require('./mongodb');
-const connectionObject = require('./connectInfo');
-const connectionstring = `mongodb://${connectionObject.username}:${connectionObject.password}@ds143614.mlab.com:43614/interstellardb`;
+const username = process.env.username;
+const password = process.env.password;
+const connectionstring = `mongodb://${username}:${password}@ds143614.mlab.com:43614/interstellardb`;
 const database = new Database(connectionstring);
 
 module.exports = database;
